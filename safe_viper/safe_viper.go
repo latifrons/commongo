8f9fbfef419e7a6f6e8551ec3605718903a1b8a1
latifrons <latifrons88@gmail.com>
2025-08-19 09:57:25 +0800

    viper get default

--- safe_viper/safe_viper.go
+++ safe_viper/safe_viper.go
@@ -1,9 +1,10 @@
 package safe_viper
 
 import (
+	"time"
+
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
-	"time"
 )
 
 func ViperMustGetString(key string) string {
@@ -41,5 +42,44 @@ func ViperMustGetDuration(key string) time.Duration {
 		logrus.WithField("key", key).Fatal("config invalid")
 	}
 	return td
+}
 
+func ViperGetStringOrDefault(key, defaultValue string) string {
+	if !viper.IsSet(key) || viper.GetString(key) == "" {
+		return defaultValue
+	}
+	return viper.GetString(key)
+}
+
+func ViperGetIntOrDefault(key string, defaultValue int) int {
+	if !viper.IsSet(key) || viper.GetString(key) == "" {
+		return defaultValue
+	}
+	return viper.GetInt(key)
+}
+
+func ViperGetBoolOrDefault(key string, defaultValue bool) bool {
+	if !viper.IsSet(key) || viper.GetString(key) == "" {
+		return defaultValue
+	}
+	return viper.GetBool(key)
+}
+
+func ViperGetFloat64OrDefault(key string, defaultValue float64) float64 {
+	if !viper.IsSet(key) || viper.GetString(key) == "" {
+		return defaultValue
+	}
+	return viper.GetFloat64(key)
+}
+
+func ViperGetDurationOrDefault(key string, defaultValue time.Duration) time.Duration {
+	if !viper.IsSet(key) || viper.GetString(key) == "" {
+		return defaultValue
+	}
+	s := viper.GetString(key)
+	td, err := time.ParseDuration(s)
+	if err != nil {
+		logrus.WithField("key", key).Fatal("config invalid")
+	}
+	return td
 }
